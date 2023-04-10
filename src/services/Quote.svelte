90c5181ee445a58124e8225ce262aa3365a086c0
Michael Thomas <michaelthomas0721@icloud.com>
2023-04-10 19:25:32 -0400

    Feat: File restructuring

--- src/services/Quote.svelte
+++
@@ -1,8 +0,0 @@
-<!-- Quote.svelte -->
-<script lang="ts">
-  export let quoteText;
-</script>
-
-<div class="quote">
-  <p>{quoteText[0].props.text[0]}</p>
-</div>
--- src/services/RichTextRenderer.ts
+++ src/services/RichTextRenderer.ts
@@ -1,42 +1,47 @@
-import { BLOCKS, INLINES } from '@contentful/rich-text-types';
+import { BLOCKS, INLINES } from "@contentful/rich-text-types";
 
-import BlockEntry from '$components/posts/BlockEntry.svelte';
-import BlockImage from '$components/posts/BlockImage.svelte';
-import BlockQuote from '$components/posts/BlockQuote.svelte';
-import Heading from './Heading.svelte';
-import InlineHyperLink from '$components/posts/InlineHyperLink.svelte';
-import Paragraph from './Paragraph.svelte';
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import BlockEntry from "$components/posts/BlockEntry.svelte";
+import BlockImage from "$components/posts/BlockImage.svelte";
+import BlockQuote from "$components/posts/BlockQuote.svelte";
+import Heading from "$components/posts/Heading.svelte";
+import InlineHyperLink from "$components/posts/InlineHyperLink.svelte";
+import Paragraph from "$components/posts/Paragraph.svelte";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
 export default function renderRichText(content, inlineContent) {
   const options = {
     renderNode: {
       [BLOCKS.PARAGRAPH]: (node, children) => ({
         component: Paragraph,
-        props: { text: children }
+        props: { text: children },
       }),
       [BLOCKS.HEADING_1]: (node, children) => ({
         component: Heading,
-        props: { level: 1, text: children }
+        props: { level: 1, text: children },
       }),
       [BLOCKS.QUOTE]: (node, children) => ({
         component: BlockQuote,
-        props: { quoteText: children }
+        props: { quoteText: children },
       }),
       [BLOCKS.EMBEDDED_ASSET]: (node) => ({
         component: BlockImage,
-        props: { image: node.data.target }
+        props: { image: node.data.target },
       }),
       [BLOCKS.EMBEDDED_ENTRY]: (node) => ({
         component: BlockEntry,
-        props: { node: node, inlineContent: inlineContent.find((cnt) => cnt?.sys?.id == node?.data?.target?.sys?.id) }
+        props: {
+          node: node,
+          inlineContent: inlineContent.find(
+            (cnt) => cnt?.sys?.id == node?.data?.target?.sys?.id
+          ),
+        },
       }),
       [INLINES.HYPERLINK]: (node, children) => ({
         component: InlineHyperLink,
         props: { href: node.data.uri, text: children },
-      })
+      }),
       // add more block types here as needed
-    } as any
+    } as any,
   };
 
   return documentToReactComponents(content, options) as any;
